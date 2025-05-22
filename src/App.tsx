f6f4526df27ea5a9b29371067208b4edf13ab6c9
Joel Chi <abongsjoel@gmail.com>
2025-05-22 14:52:41 +0100

    Extract dashboard to component of its own

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
-import ReplyBox from "./components/Messages/ReplyBox";
-import Messages from "./components/Messages";
-import Users from "./components/Users";
-import EmptyChat from "./components/Messages/EmptyChat";
-import MessageHeader from "./components/Messages/MessageHeader";
-
-import type { UserId } from "./types";
+import Dashboard from "./components/Dashboard";
 
 import "./App.scss";
 
 function App() {
-  const [selectedUserId, setSelectedUserId] = useState<UserId>(null);
-
   return (
-    <main className="dashboard">
+    <>
       <Toaster
         position="top-right"
         toastOptions={{
@@ -26,24 +17,8 @@ function App() {
           },
         }}
       />
-
-      <Users
-        selectedUserId={selectedUserId}
-        setSelectedUserId={setSelectedUserId}
-      />
-
-      <section className="chat-area">
-        {selectedUserId ? (
-          <>
-            <MessageHeader selectedUserId={selectedUserId} />
-            <Messages selectedUserId={selectedUserId} />
-            <ReplyBox selectedUserId={selectedUserId} />
-          </>
-        ) : (
-          <EmptyChat />
-        )}
-      </section>
-    </main>
+      <Dashboard />
+    </>
   );
 }
 
--- src/redux/apis/messagesApi.ts
+++ src/redux/apis/messagesApi.ts
@@ -1,30 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { ChatMessage, UserId } from "../../types";
 
-// export const messagesApi = createApi({
-//   reducerPath: "messagesApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:5173",
-//   }),
-//   tagTypes: ["Messages"],
-//   endpoints: (builder) => ({
-//     getUserMessages: builder.query<ChatMessage[], UserId>({
-//       query: (userId) => `api/messages/${userId}`,
-//       providesTags: (_, __, userId) =>
-//         userId ? [{ type: "Messages", id: userId }] : [],
-//     }),
-//     sendMessage: builder.mutation<void, { to: UserId; message: string }>({
-//       query: ({ to, message }) => ({
-//         url: "api/send-message",
-//         method: "POST",
-//         body: { to, message },
-//       }),
-//       invalidatesTags: (_, __, arg) =>
-//         arg?.to ? [{ type: "Messages", id: arg.to }] : [],
-//     }),
-//   }),
-// });
-
 export const messagesApi = createApi({
   reducerPath: "messagesApi",
   baseQuery: fetchBaseQuery({
