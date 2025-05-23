563867068bc4f0272f7443fee830edbc31a7c4bd
Joel Chi <abongsjoel@gmail.com>
2025-05-24 00:12:11 +0100

    Make time relative

--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -1,4 +1,12 @@
 import React, { useEffect, useRef } from "react";
+import {
+  differenceInMinutes,
+  formatDistanceToNow,
+  format,
+  isToday,
+  isYesterday,
+  isThisWeek,
+} from "date-fns";
 
 import { useGetUserMessagesQuery } from "../../redux/apis/messagesApi";
 import MessagesSkeleton from "./MessagesSkeleton";
@@ -10,6 +18,25 @@ import "./Messages.scss";
 interface MessagesProps {
   selectedUser: User;
 }
+
+const getFormattedTime = (timestamp: Date): string => {
+  const date = new Date(timestamp);
+  const minutesAgo = differenceInMinutes(new Date(), date);
+
+  if (minutesAgo < 1) {
+    return "Just now";
+  } else if (minutesAgo <= 5) {
+    return formatDistanceToNow(date, { addSuffix: true }); // e.g. "5 minutes ago"
+  } else if (isToday(date)) {
+    return format(date, "p"); // e.g. "2:14 PM"
+  } else if (isYesterday(date)) {
+    return `Yesterday at ${format(date, "p")}`;
+  } else if (isThisWeek(date)) {
+    return `${format(date, "EEEE")} at ${format(date, "p")}`; // e.g. Wednesday at 2:14 PM
+  } else {
+    return format(date, "PP 'at' p");
+  }
+};
 
 const Messages: React.FC<MessagesProps> = ({
   selectedUser: { userId, userName },
@@ -52,10 +79,11 @@ const Messages: React.FC<MessagesProps> = ({
             </strong>{" "}
             {msg.content}
             <div className="msg-time">
-              {new Date(msg.timestamp).toLocaleTimeString([], {
+              {/* {new Date(msg.timestamp).toLocaleTimeString([], {
                 hour: "2-digit",
                 minute: "2-digit",
-              })}
+              })} */}
+              {getFormattedTime(msg.timestamp)}
             </div>
           </article>
         ))
