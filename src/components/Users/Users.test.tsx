98425a5b38b75516f2fe15773c0a9e995d7fc2cd
Joel Chi <abongsjoel@gmail.com>
2025-06-12 01:27:23 +0100

    Add unit test for PrivateRoute component

--- src/components/Users/Users.test.tsx
+++ src/components/Users/Users.test.tsx
@@ -1,4 +1,3 @@
-// src/components/Users/Users.test.tsx
 import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, it, vi, beforeEach, expect } from "vitest";
 import { Provider } from "react-redux";
