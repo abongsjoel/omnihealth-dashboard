7edfe67b9aa4e2123eb4becb8b36afffea94fb9e
Joel Chi <abongsjoel@gmail.com>
2025-09-18 00:56:55 +0100

    Fix failing test

--- src/components/common/Modal/Modal.test.tsx
+++ src/components/common/Modal/Modal.test.tsx
@@ -1,7 +1,20 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 
 import Modal from "../Modal";
+
+beforeEach(() => {
+  const modalRoot = document.createElement("div");
+  modalRoot.setAttribute("id", "modal-root");
+  document.body.appendChild(modalRoot);
+});
+
+afterEach(() => {
+  const modalRoot = document.getElementById("modal-root");
+  if (modalRoot) {
+    document.body.removeChild(modalRoot);
+  }
+});
 
 describe("Modal Component", () => {
   const modalContent = <div data-testid="modal-content">Hello Modal</div>;
